344801d121e94695e00f31e5864031a1201bb3a9
kirananarase <kiran.anarase@gmail.com>
2023-05-14 23:14:34 +0800

    surefire plugin updated

--- src/test/java/steps/DeleteStepDef.java
+++ src/test/java/steps/DeleteStepDef.java
@@ -1,9 +1,7 @@
 package steps;
-
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
 import pages.DeletePage;
 import pages.HomePage;
--- src/test/java/steps/EditStepDef.java
+++ src/test/java/steps/EditStepDef.java
@@ -1,10 +1,10 @@
 package steps;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.TestInstance;
 import pages.EditPage;
 import pages.HomePage;
 
